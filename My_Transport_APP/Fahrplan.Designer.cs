a9d0639e00cf61765696347928bc8f1ef823e50c
WimsenOG <77049446+WimsenOG@users.noreply.github.com>
2021-01-08 15:52:42 +0100

    Doc

--- My_Transport_APP/Fahrplan.Designer.cs
+++ My_Transport_APP/Fahrplan.Designer.cs
@@ -34,23 +34,30 @@
             this.coBoxNach = new System.Windows.Forms.ComboBox();
             this.btnSucheAbfahrtstaffel = new System.Windows.Forms.Button();
             this.dataGridViewVerbindungen = new System.Windows.Forms.DataGridView();
-            this.dataGridViewAbfahrtsTafel = new System.Windows.Forms.DataGridView();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.labelAbfahrtstaffel = new System.Windows.Forms.Label();
             this.Von = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nach = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Abfahrt = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Gleis = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Verspätung = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewAbfahrtsTafel = new System.Windows.Forms.DataGridView();
+            this.coBoxAbfahrtstaffel = new System.Windows.Forms.ComboBox();
+            this.labelAbfahrtstaffel = new System.Windows.Forms.Label();
+            this.Von_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nach_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Abfahrt_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Zug = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProgrammBeenden = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVerbindungen)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAbfahrtsTafel)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSucheVerbindung
             // 
-            this.btnSucheVerbindung.Location = new System.Drawing.Point(307, 51);
+            this.btnSucheVerbindung.Location = new System.Drawing.Point(360, 45);
             this.btnSucheVerbindung.Name = "btnSucheVerbindung";
-            this.btnSucheVerbindung.Size = new System.Drawing.Size(75, 23);
+            this.btnSucheVerbindung.Size = new System.Drawing.Size(105, 23);
             this.btnSucheVerbindung.TabIndex = 0;
             this.btnSucheVerbindung.Text = "Suchen";
             this.btnSucheVerbindung.UseVisualStyleBackColor = true;
@@ -58,9 +65,9 @@
             // 
             // dateTimePickerVerbindungsSuche
             // 
-            this.dateTimePickerVerbindungsSuche.Location = new System.Drawing.Point(12, 52);
+            this.dateTimePickerVerbindungsSuche.Location = new System.Drawing.Point(12, 46);
             this.dateTimePickerVerbindungsSuche.Name = "dateTimePickerVerbindungsSuche";
-            this.dateTimePickerVerbindungsSuche.Size = new System.Drawing.Size(289, 22);
+            this.dateTimePickerVerbindungsSuche.Size = new System.Drawing.Size(342, 22);
             this.dateTimePickerVerbindungsSuche.TabIndex = 2;
             // 
             // coBoxVon
@@ -68,7 +75,7 @@
             this.coBoxVon.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.coBoxVon.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.coBoxVon.FormattingEnabled = true;
-            this.coBoxVon.Location = new System.Drawing.Point(12, 12);
+            this.coBoxVon.Location = new System.Drawing.Point(48, 16);
             this.coBoxVon.Name = "coBoxVon";
             this.coBoxVon.Size = new System.Drawing.Size(182, 24);
             this.coBoxVon.TabIndex = 3;
@@ -77,21 +84,25 @@
             // 
             // coBoxNach
             // 
+            this.coBoxNach.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.coBoxNach.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.coBoxNach.FormattingEnabled = true;
-            this.coBoxNach.Location = new System.Drawing.Point(200, 12);
+            this.coBoxNach.Location = new System.Drawing.Point(283, 16);
             this.coBoxNach.Name = "coBoxNach";
             this.coBoxNach.Size = new System.Drawing.Size(182, 24);
             this.coBoxNach.TabIndex = 4;
             this.coBoxNach.SelectedIndexChanged += new System.EventHandler(this.coBoxNach_SelectedIndexChanged);
+            this.coBoxNach.TextChanged += new System.EventHandler(this.coBoxNach_TextChanged);
             // 
             // btnSucheAbfahrtstaffel
             // 
-            this.btnSucheAbfahrtstaffel.Location = new System.Drawing.Point(987, 34);
+            this.btnSucheAbfahrtstaffel.Location = new System.Drawing.Point(259, 418);
             this.btnSucheAbfahrtstaffel.Name = "btnSucheAbfahrtstaffel";
             this.btnSucheAbfahrtstaffel.Size = new System.Drawing.Size(110, 23);
             this.btnSucheAbfahrtstaffel.TabIndex = 5;
             this.btnSucheAbfahrtstaffel.Text = "Suchen";
             this.btnSucheAbfahrtstaffel.UseVisualStyleBackColor = true;
+            this.btnSucheAbfahrtstaffel.Click += new System.EventHandler(this.btnSucheAbfahrtstaffel_Click);
             // 
             // dataGridViewVerbindungen
             // 
@@ -106,38 +117,9 @@
             this.dataGridViewVerbindungen.Name = "dataGridViewVerbindungen";
             this.dataGridViewVerbindungen.RowHeadersWidth = 51;
             this.dataGridViewVerbindungen.RowTemplate.Height = 24;
-            this.dataGridViewVerbindungen.Size = new System.Drawing.Size(677, 274);
+            this.dataGridViewVerbindungen.Size = new System.Drawing.Size(686, 274);
             this.dataGridViewVerbindungen.TabIndex = 6;
             this.dataGridViewVerbindungen.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewVerbindungen_CellContentClick);
-            // 
-            // dataGridViewAbfahrtsTafel
-            // 
-            this.dataGridViewAbfahrtsTafel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAbfahrtsTafel.Location = new System.Drawing.Point(728, 80);
-            this.dataGridViewAbfahrtsTafel.Name = "dataGridViewAbfahrtsTafel";
-            this.dataGridViewAbfahrtsTafel.RowHeadersWidth = 51;
-            this.dataGridViewAbfahrtsTafel.RowTemplate.Height = 24;
-            this.dataGridViewAbfahrtsTafel.Size = new System.Drawing.Size(366, 281);
-            this.dataGridViewAbfahrtsTafel.TabIndex = 7;
-            this.dataGridViewAbfahrtsTafel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(599, 34);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(382, 24);
-            this.comboBox3.TabIndex = 8;
-            // 
-            // labelAbfahrtstaffel
-            // 
-            this.labelAbfahrtstaffel.AutoSize = true;
-            this.labelAbfahrtstaffel.Location = new System.Drawing.Point(742, 90);
-            this.labelAbfahrtstaffel.Name = "labelAbfahrtstaffel";
-            this.labelAbfahrtstaffel.Size = new System.Drawing.Size(100, 17);
-            this.labelAbfahrtstaffel.TabIndex = 9;
-            this.labelAbfahrtstaffel.Text = "Abfahrtstaffel :";
-            this.labelAbfahrtstaffel.Click += new System.EventHandler(this.label1_Click);
             // 
             // Von
             // 
@@ -174,13 +156,110 @@
             this.Verspätung.Name = "Verspätung";
             this.Verspätung.Width = 125;
             // 
+            // dataGridViewAbfahrtsTafel
+            // 
+            this.dataGridViewAbfahrtsTafel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAbfahrtsTafel.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Von_,
+            this.Nach_,
+            this.Abfahrt_,
+            this.Zug});
+            this.dataGridViewAbfahrtsTafel.Location = new System.Drawing.Point(12, 464);
+            this.dataGridViewAbfahrtsTafel.Name = "dataGridViewAbfahrtsTafel";
+            this.dataGridViewAbfahrtsTafel.RowHeadersWidth = 51;
+            this.dataGridViewAbfahrtsTafel.RowTemplate.Height = 24;
+            this.dataGridViewAbfahrtsTafel.Size = new System.Drawing.Size(686, 419);
+            this.dataGridViewAbfahrtsTafel.TabIndex = 7;
+            this.dataGridViewAbfahrtsTafel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // coBoxAbfahrtstaffel
+            // 
+            this.coBoxAbfahrtstaffel.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.coBoxAbfahrtstaffel.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.coBoxAbfahrtstaffel.FormattingEnabled = true;
+            this.coBoxAbfahrtstaffel.Location = new System.Drawing.Point(12, 417);
+            this.coBoxAbfahrtstaffel.Name = "coBoxAbfahrtstaffel";
+            this.coBoxAbfahrtstaffel.Size = new System.Drawing.Size(241, 24);
+            this.coBoxAbfahrtstaffel.TabIndex = 8;
+            this.coBoxAbfahrtstaffel.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            this.coBoxAbfahrtstaffel.TextChanged += new System.EventHandler(this.coBoxAbfahrtstaffel_TextChanged);
+            // 
+            // labelAbfahrtstaffel
+            // 
+            this.labelAbfahrtstaffel.AutoSize = true;
+            this.labelAbfahrtstaffel.Location = new System.Drawing.Point(12, 388);
+            this.labelAbfahrtstaffel.Name = "labelAbfahrtstaffel";
+            this.labelAbfahrtstaffel.Size = new System.Drawing.Size(100, 17);
+            this.labelAbfahrtstaffel.TabIndex = 9;
+            this.labelAbfahrtstaffel.Text = "Abfahrtstaffel :";
+            this.labelAbfahrtstaffel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Von_
+            // 
+            this.Von_.HeaderText = "Von";
+            this.Von_.MinimumWidth = 6;
+            this.Von_.Name = "Von_";
+            this.Von_.Width = 125;
+            // 
+            // Nach_
+            // 
+            this.Nach_.HeaderText = "Nach";
+            this.Nach_.MinimumWidth = 6;
+            this.Nach_.Name = "Nach_";
+            this.Nach_.Width = 125;
+            // 
+            // Abfahrt_
+            // 
+            this.Abfahrt_.HeaderText = "Abfahrt";
+            this.Abfahrt_.MinimumWidth = 6;
+            this.Abfahrt_.Name = "Abfahrt_";
+            this.Abfahrt_.Width = 125;
+            // 
+            // Zug
+            // 
+            this.Zug.HeaderText = "Zug";
+            this.Zug.MinimumWidth = 6;
+            this.Zug.Name = "Zug";
+            this.Zug.Width = 125;
+            // 
+            // ProgrammBeenden
+            // 
+            this.ProgrammBeenden.Location = new System.Drawing.Point(623, 889);
+            this.ProgrammBeenden.Name = "ProgrammBeenden";
+            this.ProgrammBeenden.Size = new System.Drawing.Size(75, 23);
+            this.ProgrammBeenden.TabIndex = 10;
+            this.ProgrammBeenden.Text = "Schliessen";
+            this.ProgrammBeenden.UseVisualStyleBackColor = true;
+            this.ProgrammBeenden.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Von";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nach";
+            // 
             // Fahrplan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1092, 1035);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProgrammBeenden);
             this.Controls.Add(this.labelAbfahrtstaffel);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.coBoxAbfahrtstaffel);
             this.Controls.Add(this.dataGridViewAbfahrtsTafel);
             this.Controls.Add(this.dataGridViewVerbindungen);
             this.Controls.Add(this.btnSucheAbfahrtstaffel);
@@ -189,7 +268,7 @@
             this.Controls.Add(this.dateTimePickerVerbindungsSuche);
             this.Controls.Add(this.btnSucheVerbindung);
             this.Name = "Fahrplan";
-            this.Text = "Fahrplan";
+            this.Text = "er";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVerbindungen)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAbfahrtsTafel)).EndInit();
             this.ResumeLayout(false);
@@ -206,12 +285,19 @@
         private System.Windows.Forms.Button btnSucheAbfahrtstaffel;
         private System.Windows.Forms.DataGridView dataGridViewVerbindungen;
         private System.Windows.Forms.DataGridView dataGridViewAbfahrtsTafel;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox coBoxAbfahrtstaffel;
         private System.Windows.Forms.Label labelAbfahrtstaffel;
         private System.Windows.Forms.DataGridViewTextBoxColumn Von;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nach;
         private System.Windows.Forms.DataGridViewTextBoxColumn Abfahrt;
         private System.Windows.Forms.DataGridViewTextBoxColumn Gleis;
         private System.Windows.Forms.DataGridViewTextBoxColumn Verspätung;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Von_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nach_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Abfahrt_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Zug;
+        private System.Windows.Forms.Button ProgrammBeenden;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- My_Transport_APP/Fahrplan.cs
+++ My_Transport_APP/Fahrplan.cs
@@ -34,7 +34,7 @@ namespace My_Transport_APP
             
 
         }
-
+        //Such Button für die Verbindungstaffel
         private void btnSucheVerbindung_Click(object sender, EventArgs e)
         {
             dataGridViewVerbindungen.Rows.Clear();
@@ -45,7 +45,7 @@ namespace My_Transport_APP
                 dataGridViewVerbindungen.Rows.Add(coBoxVon.Text, coBoxNach.Text, StationConFoch.From.Departure,StationConFoch.From.Platform,StationConFoch.From.Delay+" min");
             }
         }
-
+        //Für die Vorschläge während des Tippens(Von wo)
         private void coBoxVon_TextChanged(object sender, EventArgs e)
         {
             if (coBoxVon.Text != "")
@@ -65,6 +65,16 @@ namespace My_Transport_APP
 
         private void coBoxNach_SelectedIndexChanged(object sender, EventArgs e)
         {
+        }
+
+        private void dataGridViewVerbindungen_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        //Für die Vorschläge während des Tippens(Nach wohin)
+        private void coBoxNach_TextChanged(object sender, EventArgs e)
+        {
+
             if (coBoxNach.Text != "")
             {
                 var transport = new Transport();
@@ -80,9 +90,46 @@ namespace My_Transport_APP
             }
         }
 
-        private void dataGridViewVerbindungen_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
+            
+        }
+        //Für die Vorschläge während des Tippens(Abfahrttaffel)
+        private void coBoxAbfahrtstaffel_TextChanged(object sender, EventArgs e)
+        {
+            if (coBoxAbfahrtstaffel.Text != "")
+            {
+                var transport = new Transport();
+                var Statios = transport.GetStations(coBoxAbfahrtstaffel.Text).StationList;
 
+                var stationsAutoComplete = new AutoCompleteStringCollection();
+                foreach (var stationForech in Statios)
+                {
+                    stationsAutoComplete.Add(stationForech.Name);
+                }
+
+                coBoxAbfahrtstaffel.AutoCompleteCustomSource = stationsAutoComplete;
+            }
+        }
+        //Such Button für Abfahrtstaffel
+        private void btnSucheAbfahrtstaffel_Click(object sender, EventArgs e)
+        {
+            dataGridViewAbfahrtsTafel.Rows.Clear();
+            var transport = new Transport();
+            var Stations = transport.GetStations(coBoxAbfahrtstaffel.Text).StationList[0];
+            var Stationconnection = transport.GetStationBoard(coBoxAbfahrtstaffel.Text,Stations.Id);
+            foreach (var StationConFoch in Stationconnection.Entries)
+            {
+                dataGridViewAbfahrtsTafel.Rows.Add(coBoxAbfahrtstaffel.Text,StationConFoch.To,StationConFoch.Stop.Departure, StationConFoch.Name);
+            }
+        }
+        //Das Programm mit einem Button Beenden
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bist du Sicher ?", "Schliessen", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
         }
     }
            
